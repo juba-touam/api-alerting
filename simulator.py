25b7e1c98bbf6954c028a7a0cb3c60a547418911
touam <juba.touam@etu.unistra.fr>
2025-04-22 23:18:35 +0200

    add deploy&ingress

--- simulator.py
+++ simulator.py
@@ -8,17 +8,17 @@ stop_simulation = Event()
 def run_simulation(count, pause):
     for i in range(count):
         if stop_simulation.is_set():
-            print(f"[{i}] ❌ Simulation arrêtée.")
+            print(f"[{i}] Simulation arrêtée.")
             break
 
         try:
             headers = {"Authorization": "Bearer totally.invalid.token"}
             response = requests.get(TARGET_URL, headers=headers)
             if response.status_code == 401:
-                print(f"[{i}] ✅ 401 reçu - échec d'authentification attendu")
+                print(f"[{i}] 401 reçu - échec d'authentification attendu")
             else:
-                print(f"[{i}] ⚠️ Reçu {response.status_code} - pas un échec attendu")
+                print(f"[{i}] Reçu {response.status_code} - pas un échec attendu")
         except Exception as e:
-            print(f"[{i}] ❌ Erreur : {e}")
+            print(f"[{i}] Erreur : {e}")
 
         time.sleep(pause)
